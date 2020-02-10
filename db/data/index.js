ee3969d077489aa0441160ed506b70057adff806
Connie <conniedixon106@gmail.com>
2020-02-10 12:37:37 +0000

    Added index and seed directory to db/data

---
+++ db/data/index.js
@@ -0,0 +1,10 @@
+const devPath = require("./development-data"),
+const testPath = require("./test-data")
+
+const data = {
+    development: devPath,
+    test: testPath
+  };
+  
+  module.exports = data[ENV];
+  
