fd2111aef4ba950a285c05915ff4566e741c9cad
Connie <conniedixon106@gmail.com>
2020-02-10 11:56:40 +0000

    Added dev and test data indexes with module exports in each file

---
+++ db/data/development-data/index.js
@@ -0,0 +1,6 @@
+const articles = require("./articles.js");
+const comments = require("./comments.js");
+const topics = require("./topics.js");
+const users = require("./users.js");
+
+module.exports = { articles, comments, topics, users };
