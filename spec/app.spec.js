afb8e34ac96e42d9e228524eb11bb1cdc600c292
Connie <conniedixon106@gmail.com>
2020-02-14 10:48:35 +0000

    Added 404 and 200 tests for endpoint /articles

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -61,6 +61,33 @@ describe("APP/API:", () => {
     });
   });
   describe("/ARTICLES", () => {
+    describe.only("GET", () => {
+      it("Status 200: returns an array of articles", () => {
+        return request(app)
+          .get("/api/articles")
+          .expect(200)
+          .then(({ body: { topics } }) => {
+            expect(topics).to.be.an("array");
+            expect(topics[0]).to.contain.keys(
+              "author",
+              "title",
+              "article_id",
+              "topic",
+              "created_at",
+              "votes",
+              "comment_count"
+            );
+          });
+      });
+      it("Status 404: Path not found", () => {
+        return request(app)
+          .get("/api/arcicles")
+          .expect(404)
+          .then(({ body: { msg } }) => {
+            expect(msg).to.eql("Path not found");
+          });
+      });
+    });
     describe("/:article_id", () => {
       describe("GET", () => {
         it("Status 200: Returns an object with relevant keys", () => {
