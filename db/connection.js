c38d98c92053720738dd4c2d62ab3b73b88293c0
connie <conniedixon106@gmail.com>
2020-03-01 19:45:37 +0000

    Edited bug in conneciton file for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 const knexMaker = require("knex");
+const ENV = process.env.NODE_ENV || 'development';
 
 const dbConfig =  ENV === 'production'
     ? { client: 'pg', connection: process.env.DATABASE_URL }
     : require('../knexfile');
-
+    
 const knex = knexMaker(dbConfig);
-
+ 
 
 module.exports = knex(dbConfig);
