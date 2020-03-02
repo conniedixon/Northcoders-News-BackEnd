db566a102594f549ffece46149c3414c417f3307
connie <conniedixon106@gmail.com>
2020-03-02 08:43:17 +0000

    add err to send body

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/*", (req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.code === "22P02") res.status(400).send({ msg: "Bad Request" });
   if (err.status) res.status(err.status).send({ msg: err.msg });
-  else res.status(500).send({ msg: "Internal server error" });
+  else res.status(500).send({ msg: "Internal server error" , err});
 });
 
 module.exports = app;
